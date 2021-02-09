bb4eeefec085471ae0d420683db47f0bb9b503c5
blissfulwolf <cashgrid@yahoo.com>
2021-02-09 19:16:58 +0800

    hungry prompt

--- hungry.py
+++ hungry.py
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 print("I am hungry")
 hungry = input("are you hungry")
 if hungry=="yes":
     print("eat burger")
 else:
     print("go buy food")
-=======
 print("I am hungry")
->>>>>>> f29482e050dbc44b1056451f979cb1887b0d8e4d
